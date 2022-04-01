962d12253fa7460b5ccf74fc0b16aecae80a1530
Niek <Frankenstein795@gmail.com>
2022-04-01 22:35:32 +0200

    Updated hold me now page

--- src/views/HoldMeNow.vue
+++ src/views/HoldMeNow.vue
@@ -14,9 +14,9 @@ const options: pageOptions = {
 </script>
 
 <template>
-  <img src="../assets/hold-me-now-cover.png" alt="Album cover" class="background object-cover w-full h-full">
+  <img src="../assets/hold-me-now-background.png" alt="Album cover" class="background object-cover w-full h-full">
   <div class="frame mb-12 flex flex-col rounded-2xl drop-shadow-lg overflow-hidden">
-    <img src="../assets/hold-me-now-cover.png" alt="ALBUM COVER" class="w-full"/>
+    <img src="../assets/hold-me-now-cover.jpg" alt="ALBUM COVER" class="w-full"/>
     <NameCard :type="options.releaseType" :title=options.title :text-color=options.iconColor :background-color=options.backgroundColor />
     <Card type="WATCH" url="https://www.youtube.com/watch?v=PVvxr3YJdNM" track-source="Youtube" :button-color=options.buttonColor :text-color=options.buttonTextColor :background-color=options.backgroundColor :icon-color=options.iconColor />
     <Card type="STREAM" url="https://soundcloud.com/gmafiarecords/niek-hold-me-now-original-mix" track-source="SoundCloud" :button-color=options.buttonColor :text-color=options.buttonTextColor :background-color=options.backgroundColor :icon-color=options.iconColor />
