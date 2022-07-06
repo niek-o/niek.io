1c4e8a7328fd5239700c962e8ced33b373b10ea6
Niek <Frankenstein795@gmail.com>
2022-07-06 20:22:14 +0200

    Fix font weight in Button.vue

--- components/Button.vue
+++ components/Button.vue
@@ -12,8 +12,7 @@ defineProps<{
 <template>
   <a :href=url
       target="_blank"
-      class="button text-white p-3 text-center align-middle no-underline inline-block cursor-pointer font-black text-2xl w-32 h-14"
-      :style="{ backgroundColor: backgroundColor, color: textColor }">
+      class="button p-3 text-center align-middle no-underline inline-block cursor-pointer font-bold text-2xl w-32 h-14">
     {{ text }}
   </a>
 </template>
@@ -21,5 +20,7 @@ defineProps<{
 <style scoped>
 .button {
   border-radius: 12px;
+  background-color: v-bind(backgroundColor);
+  color: v-bind(textColor);
 }
 </style>
