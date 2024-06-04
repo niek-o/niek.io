2a8c4a305020d0d4f5b625026f4f881bb32ae687
Niek <frankenstein795@gmail.com>
2024-06-04 14:38:02 +0200

    Update config to include .env at build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,9 +23,9 @@ export default defineNuxtConfig({
     port: 5174
   },
   runtimeConfig: {
-    apiKey: '', // can be overridden by NUXT_API_SECRET environment variable
+    apiKey: process.env.NUXT_API_KEY, // can be overridden by NUXT_API_SECRET environment variable
     public: {
-      cdnUrl: '' // can be overridden by NUXT_PUBLIC_API_BASE environment variable
+      cdnUrl: process.env.NUXT_PUBLIC_CDN_URL // can be overridden by NUXT_PUBLIC_API_BASE environment variable
     }
   }
 })
